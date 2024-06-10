8f4b2d79d2a85c59a7eeb3f68fc6d440ede264a9
wndrbrwn <jyh7678@hotmail.com>
2024-06-10 17:10:13 +0900

    Fin

--- vite/src/abis/contractAddress.ts
+++ vite/src/abis/contractAddress.ts
@@ -1,2 +1,2 @@
-export const mintContractAddress = "0x624BCAaFC507807d24220654B0f9d15B76eCa78d";
-export const saleContractAddress = "0xd1A4405339c9326b94B8a2a7cb0CBB5DFE7E463B";
+export const mintContractAddress = "0x5bbec9a3D37dbF8Cf244a99180eF82CDeb8C76ab";
+export const saleContractAddress = "0x1bD0bf2A1CE5E85b450A2E4FD6c7A84c3720552b";
